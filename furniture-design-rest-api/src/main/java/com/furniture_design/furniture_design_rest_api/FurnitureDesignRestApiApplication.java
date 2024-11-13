31f881560f0321654372f8fd8d5cded488b4e17f
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-11-13 08:34:59 +0000

    Spring Boot プロジェクトの生成

---
+++ furniture-design-rest-api/src/main/java/com/furniture_design/furniture_design_rest_api/FurnitureDesignRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.furniture_design.furniture_design_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureDesignRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureDesignRestApiApplication.class, args);
+	}
+
+}
---
+++ furniture-design-rest-api/src/test/java/com/furniture_design/furniture_design_rest_api/FurnitureDesignRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furniture_design.furniture_design_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureDesignRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
